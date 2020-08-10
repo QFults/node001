0808e60ae7fe8abaad77492ecfac400d43b65b2a
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-10 13:20:02 -0700

    code

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
+// let dog = require('./dog.js')
+// let cat = require('./cat.js')
 
-// let firstName = process.argv[2]
-// let lastName = process.argv[3]
-// let movie = process.argv[4]
+// console.log(dog.name)
 
-// console.log(`Their name is ${firstName} ${lastName} and their favorite movie is ${movie}`)
+let pets = require('./pets.js')
 
-let operator = process.argv[2],
-firstNum = parseInt(process.argv[3]),
-secondNum = parseInt(process.argv[4])
+let dog = pets[0]
+let cat = pets[1]
 
-switch (operator) {
-  case 'add':
-    console.log(firstNum + secondNum)
-    break
-  case 'subtract':
-    console.log(firstNum - secondNum)
-    break
-  case 'multiply':
-    console.log(firstNum * secondNum)
-    break
-  case 'divide':
-    console.log(firstNum / secondNum)
-    break
-}
+console.log(dog, cat)
