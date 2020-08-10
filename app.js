7405cfb07d83f1c5f9c29e3baee14a49358ab7e7
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-10 11:35:48 -0700

    code

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 
-console.log(`Hello ${process.argv[2]}!`)
+let firstName = process.argv[2]
+let lastName = process.argv[3]
+let movie = process.argv[4]
+
+console.log(`Their name is ${firstName} ${lastName} and their favorite movie is ${movie}`)
