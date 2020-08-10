9f06c8c51e3c376e02ef5dd8a9fc492469c8071c
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-10 13:46:15 -0700

    code

--- app.js
+++ app.js
@@ -3,9 +3,16 @@
 
 // console.log(dog.name)
 
-let pets = require('./pets.js')
+// let pets = require('./pets.js')
 
-let dog = pets[0]
-let cat = pets[1]
+// let dog = pets[0]
+// let cat = pets[1]
 
-console.log(dog, cat)
+// console.log(dog, cat)
+
+let movies = require('./favoriteMovies.js')
+
+console.log('Favorite Movies:')
+for (let i = 0; i < movies.length; i++) {
+  console.log(movies[i])
+}
